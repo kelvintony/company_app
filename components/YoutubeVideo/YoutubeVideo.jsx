5ad9ebe8ebd192fe2161ff82b8c4cdde82caba60
kelvin tony <kelvinudemy@gmail.com>
2023-08-29 18:45:54 +0100

    update 1.2

--- components/YoutubeVideo/YoutubeVideo.jsx
+++ components/YoutubeVideo/YoutubeVideo.jsx
@@ -1,15 +1,26 @@
+// import React from 'react';
+// import YouTube from 'react-youtube';
+
+// const YoutubeVideo = () => {
+//   const videoUrl =
+//     'https://www.youtube.com/watch?v=i7ul4LNTnfI&pp=ygUWaW52ZXN0bWVudCBzaG9ydCB2aWRlbw%3D%3D';
+//   const opts = {
+//     height: '390',
+//     width: '640',
+//   };
+
+//   return <YouTube videoId={videoUrl} opts={opts} />;
+// };
+
+// export default YoutubeVideo;
 import React from 'react';
-import YouTube from 'react-youtube';
 
 const YoutubeVideo = () => {
-  const videoUrl =
-    'https://www.youtube.com/watch?v=i7ul4LNTnfI&pp=ygUWaW52ZXN0bWVudCBzaG9ydCB2aWRlbw%3D%3D';
-  const opts = {
-    height: '390',
-    width: '640',
-  };
-
-  return <YouTube videoId={videoUrl} opts={opts} />;
+  return (
+    <div>
+      <h1>YoutubeVideo</h1>
+    </div>
+  );
 };
 
 export default YoutubeVideo;
