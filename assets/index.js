df75b0d45b6a2f1be34b6ab588433de493181146
kelvin tony <kelvinudemy@gmail.com>
2023-09-02 16:40:26 +0100

    update 1.23

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,38 @@
 import dp4 from './dp4.jpg';
 import upgradedLogo from './upgradedLogo.png';
 import dp4_removebg from './dp4_removebg.png';
+import partner1 from './partner1.JPG';
+import partner2 from './partner2.JPG';
+import partner3 from './partner3.JPG';
+import partner4 from './partner4.JPG';
+import partner5 from './partner5.JPG';
+import partner6 from './partner6.JPG';
+import partner7 from './partner7.JPG';
+import partner8 from './partner8.JPG';
+import partner9 from './partner9.JPG';
+import partner10 from './partner10.JPG';
+import partner11 from './partner11.JPG';
+import partner12 from './partner12.JPG';
+import partner13 from './partner13.JPG';
+import partner14 from './partner14.JPG';
+import partner15 from './partner15.JPG';
 
-export { dp4_removebg, dp4 };
+export {
+  partner1,
+  partner2,
+  partner3,
+  partner4,
+  partner5,
+  partner6,
+  partner7,
+  partner8,
+  partner9,
+  partner10,
+  partner11,
+  partner12,
+  partner13,
+  partner14,
+  partner15,
+  dp4_removebg,
+  dp4,
+};
