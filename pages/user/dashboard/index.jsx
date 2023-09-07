21644942c89b8ab8f318999d25e594c32dae9971
kelvin tony <kelvinudemy@gmail.com>
2023-09-07 20:42:26 +0100

    update 1.11

--- pages/user/dashboard/index.jsx
+++ pages/user/dashboard/index.jsx
@@ -27,7 +27,71 @@ const UserDashboard = () => {
     <DashboardLayout>
       <>
         <div className={styles.section_a}>
-          <h1>This is Dashboard</h1>
+          <h3>Hello {state?.user?.fullName}!</h3>
+          <div className={styles.dashboard_container}>
+            <div className={styles.left_panel}>
+              <div className={styles.profit_target}>
+                <p>29.98%</p>
+                <p>Profit target for this month</p>
+              </div>
+
+              <div className={styles.match_event}>
+                <p>Football</p>
+                <p>BURNLEY VS MANCHESTER CITY</p>
+                <p>
+                  20:00 GMT+1 <br /> 11/08
+                </p>
+
+                <div className={styles.event_computations}>
+                  <div className={styles.computation_container}>
+                    <div className={styles.computation_wrapper}>
+                      <div className={styles.wrapper_innera}>
+                        <p>Event 1</p>
+                        <p>0v2.5</p>
+                      </div>
+                      <div className={styles.wrapper_innerb}>
+                        <p>Event 2</p>
+                        <p>Un2.5</p>
+                      </div>
+                    </div>
+                    <div className={styles.computation_wrapper}>
+                      <div className={styles.wrapper_innera}>
+                        <p>Total equity: &#36;512.2</p>
+                      </div>
+                      <div className={styles.wrapper_innerb}>
+                        <p>Total equity: &#36;487.8</p>
+                      </div>
+                    </div>
+                    <div className={styles.computation_wrapper}>
+                      <div className={styles.wrapper_innera}>
+                        <p>Expected returns: &#36;1,010</p>
+                      </div>
+                      <div className={styles.wrapper_innerb}>
+                        <p>Expected returns: &#36;1,010</p>
+                      </div>
+                    </div>
+                    <div className={styles.computation_wrapper}>
+                      <div className={styles.wrapper_innera}>
+                        <p>Event RO1: &#36;910</p>
+                      </div>
+                      <div className={styles.wrapper_innerb}>
+                        <p>Event RO1: &#36;910</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <button className={styles.btn_stake}>Trade Now</button>
+                </div>
+              </div>
+            </div>
+            <div className={styles.right_panel}>
+              <p>Financial Analytics</p>
+              <p>Total Balance: &#36;1,300 </p>
+              <p>Equity: &#36;1,000 </p>
+              <p>ROI: &#36;300 </p>
+              <p>Withdrawable Balance: &#36;300 </p>
+            </div>
+          </div>
         </div>
       </>
     </DashboardLayout>
