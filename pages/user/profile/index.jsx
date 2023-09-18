c217f8994ad65cff2ada578b3238c60ef3de033e
kelvin tony <kelvinudemy@gmail.com>
2023-09-18 17:06:21 +0100

    update 1.22

--- pages/user/profile/index.jsx
+++ pages/user/profile/index.jsx
@@ -4,55 +4,61 @@ import DashboardLayout from '../../../components/DashBoardLayout/DashboardLayout
 import { BsPersonCircle } from 'react-icons/bs';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import UserLoader from '../../../components/UserLoader/UserLoader';
 
 const Profile = () => {
-  const [userProfile, setUserProfile] = useState({});
+  const [userProfile, setUserProfile] = useState(null);
 
   useEffect(() => {
     const fetchProfile = async () => {
       try {
         const res = await axios.get('/api/customers/profile-details');
-        console.log('from profile', res);
         if (res) {
           setUserProfile(res.data);
         }
       } catch (error) {
-        console.log(error.message);
+        console.log('');
       }
     };
 
     fetchProfile();
   }, []);
-  console.log(userProfile);
   return (
     <DashboardLayout>
       <h3 className={styles.header}>
         Profile Details
         <BsPersonCircle className={styles.comming_soon_icon} />
       </h3>
-
-      <div className={styles.profile_container}>
-        <div className={styles.profile_items}>
-          <p>Full Name:</p>
-          <p>{userProfile?.fullName}</p>
+      {!userProfile && (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '30px',
+          }}
+        >
+          <UserLoader />
         </div>
-        <div className={styles.profile_items}>
-          <p>Email:</p>
-          <p>{userProfile?.email}</p>
+      )}
+      {userProfile && (
+        <div className={styles.profile_container}>
+          <div className={styles.profile_items}>
+            <p>Full Name:</p>
+            <p>{userProfile?.fullName}</p>
+          </div>
+          <div className={styles.profile_items}>
+            <p>Email:</p>
+            <p>{userProfile?.email}</p>
+          </div>
+
+          <div className={styles.profile_items}>
+            <p>Wallet Address:</p>
+            <p>USDT - {userProfile?.walletAddress}</p>
+          </div>
+
+          <button className={styles.edit_wallet}>Edit Wallet Address</button>
         </div>
-
-        {/* <div className={styles.profile_items}>
-          <p>Phone Number:</p>
-          <p>09064484416</p>
-        </div> */}
-
-        <div className={styles.profile_items}>
-          <p>Wallet Address:</p>
-          <p>USDT - {userProfile.walletAddress}</p>
-        </div>
-
-        <button className={styles.edit_wallet}>Edit Wallet Adress</button>
-      </div>
+      )}
     </DashboardLayout>
   );
 };
