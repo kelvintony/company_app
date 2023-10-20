8623db5751898f59497fdd8efb4ca91579f1aa0c
kelvin tony <kelvinudemy@gmail.com>
2023-10-20 20:42:26 +0100

    update 1.22

--- pages/user/profile/index.jsx
+++ pages/user/profile/index.jsx
@@ -14,8 +14,6 @@ const Profile = () => {
 
   const [buttonLoader, setButtonLoader] = useState(false);
 
-  const [userId, setUserId] = useState(false);
-
   useEffect(() => {
     fetchProfile();
   }, []);
